9b11d12d03721ed48c21ee142a5483473a678363
Dario Reis <guttyerresjunior1995@gmail.com>
2023-05-31 17:26:11 -0300

    feat:creating the project page

---
+++ app/components/pages/projects/projects-list/project-card/index.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import Capa from '../../../../../../public/images/exemploprojeto.png'
+
+export const ProjectCard = () => {
+  return (
+    <div className="rounded-lg h-[436px] flex flex-col bg-gray-800 overflow-hidden border-2 border-gray-800 hover:border-purple-500 opacity-70 hover:opacity-100 transition-all group">
+      <div className="w-full h-48 overflow-hidden">
+        <Image
+          width={380}
+          height={200}
+          src={Capa}
+          alt="Thumbnail do projeto MarioBross"
+          unoptimized
+          className="w-full h-full object-cover group-hover:scale-110 duration-500 transition-all"
+        />
+      </div>
+
+      <div className="flex-1 flex flex-col p-8">
+        <strong className="font-medium text-gray-50/90 group-hover:text-purple-500 transition-all">
+          MarioBross
+        </strong>
+        <p className="mt-2 text-gray-400 line-clamp-4">
+          BookWise é uma plataforma de avaliação de livros que foi desenvolvida
+          durante o bootcamp Ignite da Rocketseat. Com apenas um Figma
+          precisavamos desenvolver essa aplicação completa Full Stack com
+          Next.js.
+        </p>
+        <span className="text-gray-300 text-sm font-medium block mt-auto truncate">
+          Next,js, Next Auth, Stiches
+        </span>
+      </div>
+    </div>
+  )
+}
