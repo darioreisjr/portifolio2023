b090b39b0bf2c02cb003d8dee907d2b2440a97a0
Dario Reis <guttyerresjunior1995@gmail.com>
2023-06-08 22:57:13 -0300

    refactor: changing footer icon

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,11 +1,11 @@
-import { BsFillHeartFill } from 'react-icons/Bs'
+import { FaHeart } from 'react-icons/fa'
 
 export const Footer = () => {
   return (
     <footer className="h-14 w-full flex items-center justify-center bg-gray-950">
       <span className="flex items-center gap-1.5 text-xs sm:text-sm font-mono text-gray-400">
         Made by
-        <BsFillHeartFill className="text-purple-500" size={13} />
+        <FaHeart className="text-purple-500" size={13} />
         by
         <strong className="font-medium">Dario Reis</strong>
       </span>
